fdec62dbac7710158171fc4f9745f4814ce330de
vg25565 <84933862+vg25565@users.noreply.github.com>
2024-08-05 23:15:32 +0530

    Update signup.php

--- publication/signup.php
+++ publication/signup.php
@@ -76,8 +76,8 @@ if ($query->execute()) {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';  // Set the SMTP server to send through
         $mail->SMTPAuth = true;
-        $mail->Username = 'vg2556519@gmail.com'; // Your Gmail email address
-        $mail->Password = 'cuyu vljo enge rgmp'; // Your Gmail password
+        $mail->Username = 'xyz'; // Your Gmail email address
+        $mail->Password = ''; // Your Gmail password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption
         $mail->Port = 587; // TCP port to connect to
 
